3bca0f0784a499a7e84bd060c6eb34152fc7b725
Muhammad-Lutfi-RGB <muhammad.lutfi.ft24@mail.umy.ac.id>
2025-06-10 18:33:33 +0700

    membuat program main dan membuat penanda 1 yaitu awal program dan memesan array dengen 3 elemen integer

---
+++ exceptionStandard.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yang akan digunakan
+using namespace std;
+
+int main() {
+    cout << "awal program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = { 10, 20, 30 };
+        //pesan array integer 3 elemen
