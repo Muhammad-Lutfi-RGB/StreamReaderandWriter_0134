a981394a19327d29b9db19e326dba898886d3dd1
Muhammad-Lutfi-RGB <muhammad.lutfi.ft24@mail.umy.ac.id>
2025-06-10 18:45:26 +0700

    membuat exception secara manual dengan menggunakan throw

---
+++ manualException.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; //melempar sebuah integer maka
+        cout << "Pernyataan tidak akan di tidak akan di eksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan di eksekusi
+        cout << "Pengecualian akan di eksekusi" << endl;
+    }
+    catch(...) {
+        
+    }
+}
